352f4f69f960491771f6b6be3ab07220924211e5
rdr6000 <rdr6000@gmail.com>
2022-12-28 04:07:29 +0530

    test get

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import cgi
 
 class handler(BaseHTTPRequestHandler):
 
@@ -10,26 +9,5 @@ class handler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
-
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
+		print(dic)
 		return
-
-	def do_POST(self):
-		try:
-			s = self.path
-			ctype, pdict = cgi.parse_header(self.headers['content-type'])
-			print(ctype)
-			print(pdict)
-			self.end_headers()
-			# pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-			# if ctype == 'multipart/form-data':
-			# 	fields = cgi.parse_multipart(self.rfile, pdict)
-			# 	restaurant_name = fields.get('newRestaurantName')
-				
-		except:
-			print("Inside the exception block")
